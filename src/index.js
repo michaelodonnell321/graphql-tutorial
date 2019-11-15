61eb764ab7ac5cd41afabe7501ab036eebbee70a
Michael O'Donnell <gorillaejones@gmail.com>
2019-11-15 16:06:45 -0600

    adds mutation function for post to create new links

--- src/index.js
+++ src/index.js
@@ -7,12 +7,27 @@ let links = [{
     description: 'Full stack GraphQL tutorial'
 }]
 
+//idCount is how many links there are in the array, this will be used for generating new id's
+let idCount = links.length
 const resolvers = {
     //resolver needed for each typedef
     //name needs to match
     Query: {
         info: () => 'This is the API for the Hackernews Clone',
         feed: () => links,
+    },
+    Mutation: {
+        //adds new link object, adds to existing links list, then returns the newly created link
+        //args carries the arguments for the operation, url and description for the link in this case
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        }
     },
     Link: {
         id: (parent) => parent.id,
