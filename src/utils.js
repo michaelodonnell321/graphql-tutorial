a1e74d2ae800efca1dd7df6ec187fde13d269166
Michael O'Donnell <gorillaejones@gmail.com>
2019-11-19 12:11:52 -0600

    adds imports to mutation.js

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken')
 const APP_SECRET = 'CorrectHorseBatteryStaple'
 
+//helper function that is called in resolvers which require authentication
+//if not successful, not authenticated returned
 function getUserId(context) {
     const Authorization = context.request.get('Authorization')
     if (Authorization) {
