2b65bb4b1e22a305e2bb8a43e3e99e9db147bf42
Michael O'Donnell <gorillaejones@gmail.com>
2019-11-20 16:00:49 -0600

    adds filter functionality

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,19 @@
-function feed(parent, args, context, info) {
-    return context.prisma.links()
+//if no filter string is provided, the were object will be an empty object
+//if there is, object is constructed that expresses the two filter conditions above
+//where arg is used by Prisma to filter out those Link elements that don't adhere
+
+async function feed(parent, args, context, info) {
+    const where = args.filter ? {
+        OR: [
+            { description_contains: args.filter },
+            { url_contains: args.filter },
+        ],
+    } : {}
+
+    const links = await context.prisma.links({
+        where
+    })
+    return links
 }
 
 module.exports = {
